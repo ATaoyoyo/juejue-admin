7e85a0e3132f1181b33c957ed67cfc62d239c307
ATaoyoyo <1029187998@qq.com>
2021-10-24 18:53:06 +0800

    后台端口修改

--- .umirc.ts
+++ .umirc.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   },
   proxy: {
     '/api': {
-      target: 'http://127.0.0.1:7002/api',
+      target: 'http://127.0.0.1:7001/api',
       changeOrigin: true,
       pathRewrite: { '^/api': '' },
     },
