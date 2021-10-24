5bd81c15ecdbd0420c014d50536de1f12fa91145
ATaoyoyo <1029187998@qq.com>
2021-10-25 00:02:20 +0800

    button样式

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,7 @@ import { Button } from 'antd';
 import RightHeader from '@/components/RightHeader';
 
 import './style/index.less';
+import './style/button.less';
 
 export function getInitialState() {
   return {
